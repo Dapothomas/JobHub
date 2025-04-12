f01fd9f605bfd25e23b7ef5f54085a1e34825fa2
Dapothomas <idapothomas@gmail.com>
2025-04-12 05:43:33 +0100

    changes

--- job-board/nuxt.config.ts
+++ job-board/nuxt.config.ts
@@ -42,7 +42,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiUrl: process.env.NUXT_PUBLIC_API_URL  || "http://127.0.0.1:8000",
+      apiUrl: process.env.NUXT_PUBLIC_API_URL  || "https://jobhub-qqh8.onrender.com",
     },
   },
 });
